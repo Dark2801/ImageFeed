00439f325a064fe25bf08d03deda558586b48cc4
Dark2801 <ripper2801@mail.ru>
2023-08-09 08:12:34 +0300

    Используем идентификатор, и создаем возврат ячейки

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -11,8 +11,17 @@ class ImagesListViewController: UIViewController, UITableViewDelegate, UITableVi
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
+    func configCell(for cell: ImagesListCell) { }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        // метод, который из всех ячеек, зарегистрированных в таблице, возвращает ячейку по заранее выбранному идентификатору
+        let cell = tableView.dequeueReusableCell(withIdentifier: ImagesListCell.reuseIdentiFilter, for: indexPath)
+        // Чтобы работать с ячейкой как с экземпляром класса ImagesListCell, нам надо провести приведение типов
+        guard let imageListCell = cell as? ImagesListCell else {
+            return UITableViewCell()
+        }
+        configCell(for: imageListCell)
+        //Возвращаем ячейку
+        return imageListCell
     }
     @IBOutlet private var tableView: UITableView!
     override func viewDidLoad() {
