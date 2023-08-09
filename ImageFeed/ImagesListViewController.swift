466ee2e56e8162f42096acaade6fed0f08199c6b
Dark2801 <ripper2801@mail.ru>
2023-08-09 06:44:31 +0300

    Добавил два метода numberOfRowsInSection и cellForRowAt

--- ImageFeed/ImagesListViewController.swift
+++ ImageFeed/ImagesListViewController.swift
@@ -7,9 +7,19 @@
 
 import UIKit
 
-class ImagesListViewController: UIViewController {
+class ImagesListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
     @IBOutlet private var tableView: UITableView!
     override func viewDidLoad() {
+        tableView.delegate = self
+        tableView.dataSource = self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
