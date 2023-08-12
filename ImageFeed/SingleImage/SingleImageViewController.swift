beaaa1450db6dbdff59882c13180f44601b267d2
Dark2801 <ripper2801@mail.ru>
2023-08-12 18:33:48 +0300

    Создать переход с таблицы к SingleImage View
    - создать новый Swift-файл -> SingleImageViewController
    - создать класс для контроллера с таким же названием
    - добавить новый контроллер в Main.storyboard, через "+"
    - выставить класс SingleImageViewController новому контроллеру
    в interface Builder
    
    - добавляем через Interface Builder новый UIImageView
    - задаем констрейнты до layooutMargins
    - привяжем через Interface Builder аутлет на добавленный UIImageView
    - поменяем background color на заданный Figma YPBlack
    - для imageView выберем случайную картинку
    - выставим imageView в поле Content Mode значение Aspect Fill

---
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -0,0 +1,11 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Мерзликин on 12.08.2023.
+//
+
+import UIKit
+final class SingleImageViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+}
