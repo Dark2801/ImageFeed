7c8272466256491d20b6ca7e3e11519e5df38c9d
Dark2801 <ripper2801@mail.ru>
2023-08-18 20:38:22 +0300

    Создаем UIColor+Extension
    Создаем отдельный файл с перечнем статических названий
    цветов. Если в Assets есть цвет с названием "YP Black"
    то он присвоится переменной ypBlack. Если в Assets
    нет цвета с таким названием, с помощью синтаксиса ??
    укажите дефолтное значение. В таком случае
    будет присвоен предзаданный цвет UIColor.black.

---
+++ ImageFeed/Helper/UIColor+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Андрей Мерзликин on 18.08.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+    static var ypGreen: UIColor { UIColor(named: "YP Green") ?? UIColor.green}
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red}
+    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray}
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray}
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+    static var YPWhiteAlpha50: UIColor { UIColor(named: "YP White (Alpha 50)") ?? UIColor.white}
+}
