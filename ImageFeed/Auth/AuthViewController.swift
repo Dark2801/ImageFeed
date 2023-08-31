904ac013e42322202e6644fb40e892a61443a15f
Dark2801 <ripper2801@mail.ru>
2023-08-31 10:35:25 +0300

    Создал SplashViewController

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -4,18 +4,24 @@
 //
 //  Created by Андрей Мерзликин on 26.08.2023.
 //
-
 import UIKit
 
-final class AuthViewController: UIViewController {
- private let showWebViewSegueIdentifier = "ShowWebView"
-    weak var delegate: AuthViewControllerDelegate?
+protocol AuthViewControllerDelegate: AnyObject {
+    func authViewController(_ vc : AuthViewController, didAuthenticateWithCode code: String)
+}
 
+final class AuthViewController: UIViewController {
+    private let showWebViewSegueIdentifier = "ShowWebView"
+    weak var delegate: AuthViewControllerDelegate?
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == showWebViewSegueIdentifier {
             guard
-                let webViewViewController = segue.destination as? WebViewViewController
-            else { fatalError("Failed to prepare for \(showWebViewSegueIdentifier)") }
+                let webViewViewController = segue.destination as?
+                    WebViewViewController
+            else { assertionFailure("Failed to prepare for: \(showWebViewSegueIdentifier)")
+                return
+            }
             webViewViewController.delegate = self
         } else {
             super.prepare(for: segue, sender: sender)
@@ -23,11 +29,13 @@ final class AuthViewController: UIViewController {
     }
 }
 
+// MARK: - Extension
+
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
         delegate?.authViewController(self, didAuthenticateWithCode: code)
     }
-
+    
     func webViewViewControllerDidCancel(_ vc: WebViewViewController) {
         dismiss(animated: true)
     }
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -4,88 +4,97 @@
 //
 //  Created by Андрей Мерзликин on 26.08.2023.
 //
-
 import UIKit
 import WebKit
 
-fileprivate let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+protocol WebViewViewControllerDelegate: AnyObject {
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
+}
+
 final class WebViewViewController: UIViewController {
-    @IBOutlet weak var webView: WKWebView!
-    @IBOutlet private var progressView: UIProgressView!
-    @IBAction private func didTapBackButton(_ sender: Any?) {
-        delegate?.webViewViewControllerDidCancel(self)
-    }
+    @IBOutlet private weak var webView: WKWebView!
+    @IBOutlet private weak var progressView: UIProgressView!
+    
     weak var delegate: WebViewViewControllerDelegate?
+    
     override func viewDidLoad() {
-        webView.navigationDelegate = self
         super.viewDidLoad()
-        var urlComponents = URLComponents(string: unsplashAuthorizeURLString)!
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: accessKey),
-            URLQueryItem(name: "redirect_url", value: redirectURL),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: accessScope)
-        ]
-        let url = urlComponents.url!
-        let request = URLRequest(url: url)
-        webView.load(request)
+        
+        webView.navigationDelegate = self
+        
+        loadWebView()
         updateProgress()
     }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
         webView.addObserver(
             self,
             forKeyPath: #keyPath(WKWebView.estimatedProgress),
             options: .new,
             context: nil)
+        
         updateProgress()
     }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
     }
-    override func observeValue(
-        forKeyPath keyPath: String?,
-        of object: Any?,
-        change: [NSKeyValueChangeKey : Any]?,
-        context: UnsafeMutableRawPointer?
-    ){
-        if keyPath == #keyPath(WKWebView.estimatedProgress){
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey: Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == #keyPath(WKWebView.estimatedProgress) {
             updateProgress()
         } else {
-            super.observeValue(forKeyPath: keyPath,
-                               of: object,
-                               change: change,
-                               context: context)
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
         }
     }
+    
     private func updateProgress() {
         progressView.progress = Float(webView.estimatedProgress)
         progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
     }
+    
+    @IBAction private func didTapBackButton(_ sender: Any) {
+        delegate?.webViewViewControllerDidCancel(self)
+    }
 }
+
+// MARK: - Extensions
+
 extension WebViewViewController: WKNavigationDelegate {
-    func webView(
-        _ webView: WKWebView,
-        decidePolicyFor navigationAction: WKNavigationAction,
-        decisionHandler: @escaping (WKNavigationActionPolicy) -> Void
-    ) {
-        if let code = code(from: navigationAction) {
+    func webView(_ webView: WKWebView,
+                 decidePolicyFor navigationAction: WKNavigationAction,
+                 decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let code = fetchCode(from: navigationAction.request.url) {
             delegate?.webViewViewController(self, didAuthenticateWithCode: code)
             decisionHandler(.cancel)
         } else {
             decisionHandler(.allow)
         }
     }
-    
-    private func code(from navigationAction: WKNavigationAction) -> String? {
-        if
-            let url = navigationAction.request.url,
-            let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == "/oauth/authorize/native",
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code" })
-        {
+}
+
+private extension WebViewViewController {
+    func loadWebView() {
+        var components = URLComponents(string: authorizeURLString)
+        components?.queryItems = [URLQueryItem(name: "client_id", value: accessKey),
+                                  URLQueryItem(name: "redirect_uri", value: redirectURL),
+                                  URLQueryItem(name: "response_type", value: code),
+                                  URLQueryItem(name: "scope", value: accessScope)]
+        if let url = components?.url {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
+
+    func fetchCode(from url: URL?) -> String? {
+        if let url = url,
+           let components = URLComponents(string: url.absoluteString),
+           components.path == authorizationPath,
+           let codeItem = components.queryItems?.first(where: { $0.name == code }) {
             return codeItem.value
         } else {
             return nil
