c5c496001982c79744efd0fa0ff617e5998cf401
Dark2801 <ripper2801@mail.ru>
2023-08-31 16:41:34 +0300

    Изменение простановки лайков

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -16,7 +16,7 @@ extension ImagesListCell {
     func configure(image: UIImage?, date: String, isLiked: Bool) {
         cellImage.image = image
         dateLabel.text = date
-let likeImage = isLiked ? UIImage(named: "Active") : UIImage(named: "NoActive")
+let likeImage = isLiked ? UIImage(named: "Active") : UIImage(named: "No Active")
         likeButton.setImage(likeImage, for: .normal)
     }
 }
