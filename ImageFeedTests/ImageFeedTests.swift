4ddb5edf0cf850a6d0acd8ddbad04af82159a311
Dark2801 <ripper2801@mail.ru>
2023-09-27 21:04:47 +0300

    "Временный" интеграционный тест для проверки сетевого запроса

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,14 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by Андрей Мерзликин on 26.09.2023.
+//
+
+import XCTest
+@testable import ImageFeed
+final class ImageFeedTests: XCTestCase {
+    func testExample() {
+        
+    }
+}
