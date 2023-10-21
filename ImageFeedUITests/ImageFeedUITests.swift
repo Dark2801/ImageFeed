7aef086869903c7418d5663d55ed49ac799a5026
Dark2801 <ripper2801@mail.ru>
2023-10-21 14:12:58 +0300

    Написал UI test работы с лентой

--- ImageFeedUITests/ImageFeedUITests.swift
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -42,5 +42,34 @@ import XCTest
          
          XCTAssertTrue(cell.waitForExistence(timeout: 5))
      }
+     
+     func testFeed() throws {
+         let tablesQuery = app.tables
+         
+         let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+         cell.swipeUp()
+         
+         sleep(2)
+         
+         let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
+         
+         cellToLike.buttons["likeButton"].tap()
+         cellToLike.buttons["likeButton"].tap()
+         
+         sleep(2)
+         
+         cellToLike.tap()
+         
+         sleep(2)
+         
+         let image = app.scrollViews.images.element(boundBy: 0)
+         // Zoom in
+         image.pinch(withScale: 3, velocity: 1)
+         // Zoom out
+         image.pinch(withScale: 0.5, velocity: -1)
+         
+         let navBackButtonWhiteButton = app.buttons["backButton"]
+         navBackButtonWhiteButton.tap()
+     }
     
 }
